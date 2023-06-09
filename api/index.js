7933d808b138e442e9154699455539d043fe0194
linchen1987 <linchen.1987@foxmail.com>
2023-06-09 16:16:43 +0800

    1.0.1

--- api/index.js
+++ api/index.js
@@ -1,24 +1,43 @@
 const express = require('express');
-const env = require('./libs/env');
+const bodyParser = require('body-parser');
+const { Component } = require('@blocklet/sdk');
 
 const app = express();
 
 const port = process.env.BLOCKLET_PORT || process.env.PORT || 3030;
 
+app.use(bodyParser.json({ limit: '4mb' }));
+
+app.use('/api/call', async (_req, res) => {
+  try {
+    const { data } = await Component.call({
+      name: 'z2qa5KQyqGsQSDkuEW8Do25caboQbAXt4aWS7',
+      path: '/api/internal/data',
+      data: { data: 'test' },
+    });
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
 app.get('/', (req, res) => {
   res.send(`
 <div style="display:flex;flex-direction:column;align-items:center;padding:64px 0;">
-<h1 style="margin:64px 0;">
-  <span style="display:inline-block;padding:8px 24px;background:#1dc1c7;color:#fff;">Blocklet</span>
-  <span style="color:#777;">+ Express</span>
+<h1 style="margin:48px 0;">
+  <span style="display:inline-block;padding:8px 24px;background:#1dc1c7;color:#fff;">Component Call Sender</span>
 </h1>
-<pre>
-${JSON.stringify(env, null, 2)}
-</pre>
+<div style="margin: 24px">
+  Component Call Sender should be used with Component Call Receiver.
+</div>
+<div>
+  <a href="api/call">/api/call</a>
+</div>
 </div>
   `);
 });
 
 app.listen(port, () => {
-  console.log(`Blocklet app listening on port ${port}`);
+  console.log(`Component Call Sender app listening on port ${port}`);
 });
